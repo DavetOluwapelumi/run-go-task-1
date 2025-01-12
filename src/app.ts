304d0b1ec78f5faae1beaea707c141f51c6f9917
DESKTOP-M5S11J4 <titilayodavid405@gmail.com>
2025-01-13 00:15:19 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,83 @@
+// Base class Person
+class Person {
+    firstName: string;
+    lastName: string;
+    email: string;
+    occupation: string;
+// the constructor function of the object
+    constructor(firstName:string, lastName:string, email:string, occupation:string){
+        this.firstName = firstName;
+        this.lastName =lastName;
+        this.email = email;
+        this.occupation = occupation;
+    }
+// Methods of the Oject
+    book_a_car() :string{
+        return `${this.firstName} ${this.lastName} booked a car`
+    }
+    cancel_a_booking() :string{
+        return `${this.firstName} ${this.lastName} cancelled a booking`
+    }
+    make_a_payment() :string{
+        return `${this.firstName} ${this.lastName} made a payment`
+    }
+    reschedule_booking() :string{
+        return `${this.firstName} ${this.lastName} rescheduled a booking`
+    }
+    track_car_movement() :string{
+        return `${this.firstName} ${this.lastName} tracked a car movement`
+    }
+
+}
+
+// Driver inherits the properties of class Person
+class Driver extends Person{
+    // Telling typescript number will be initialised
+    rating!: number;
+
+    // constructor function for driver
+    constructor(firstName:string, lastName:string, email: string){
+        super(firstName, lastName, email, "Driver")
+        this.rating = Math.random() * 5;
+    }
+
+    // Driver methods
+    introduce(): string{
+        return `I am ${this.firstName} ${this.lastName} a ${this.occupation} with a rating of ${this.rating.toFixed(1)}`
+    }
+    request_payment(): string{
+        return`${this.firstName} ${this.lastName} requested for payment`
+    }
+}
+
+// Student inherits the properties of class Person
+class Student extends Person{
+    constructor(firstName: string, lastName: string, email:string){
+        super(firstName, lastName, email, "student");
+    }
+    
+}
+// Admin inherits the properties of class Person
+class Admin extends Person{
+    constructor(firstName: string, lastName: string, email:string){
+        super(firstName, lastName, email, "admin");
+
+    }
+}
+
+// Testing the student Class
+let student1 = new Student("David", "Titilayo", "titilayodavid405@gmail.com")
+console.log(student1.make_a_payment());
+console.log(student1.book_a_car());
+console.log(student1.cancel_a_booking());
+
+
+// Testing the Driver Class
+let driver1 = new Driver("Bolu", "Bassey", "bolu@gmail.com")
+console.log(driver1.introduce());
+console.log(driver1.request_payment());
+
+// Testing the Admin Class
+let admin = new Admin("Emmanuel", "Lucky", "emmanuel@gmail.com")
+console.log(student1.reschedule_booking());
+console.log(student1.track_car_movement());
